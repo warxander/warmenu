f474f04f7ae7fe6d0aef7677f10f8b021b67d07d
WarBat <dikanchukov@mail.ru>
2017-08-20 00:58:34 +0300

    Better default subText color

--- warmenu.lua
+++ warmenu.lua
@@ -196,7 +196,7 @@ function WarMenu.CreateMenu(id, title)
     menus[id].titleBackgroundColor = { r = 245, g = 127, b = 23, a = 255 }
 
     menus[id].menuTextColor = { r = 255, g = 255, b = 255, a = 255 }
-    menus[id].menuSubTextColor = { r = 255, g = 255, b = 255, a = 255 }
+    menus[id].menuSubTextColor = { r = 189, g = 189, b = 189, a = 255 }
     menus[id].menuFocusTextColor = { r = 0, g = 0, b = 0, a = 255 }
     menus[id].menuFocusBackgroundColor = { r = 245, g = 245, b = 245, a = 255 }
     menus[id].menuBackgroundColor = { r = 0, g = 0, b = 0, a = 160 }
